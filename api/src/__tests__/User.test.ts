96eea3cdb48495acba5780ea46dfdb813c72521e
RogerMKobus <roger.kobus@gmail.com>
2021-02-24 15:00:05 -0300

    Criando surveys e testes com jest

---
+++ api/src/__tests__/User.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest'
+import { app } from '../app'
+
+import createConnection from '../database'
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            email:"example@email.com",
+            name:"name"
+        })
+    expect(response.status).toBe(201)
+    })
+
+    it("Should not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users").send({
+            email:"example@email.com",
+            name:"name"
+        })
+    expect(response.status).toBe(400)
+    })
+})
